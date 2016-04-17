b3605e9d52d0b81dae17351d76be64130df2e8bb
eped22 <eped22@gmail.com>
2016-04-17 01:39:18 -0400

    update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -10,21 +10,22 @@
   })
 })();
 
-$(document).ready(function () {
-    // bind click event to all internal page anchors
-    $('a[href*="#"]').on('click', function (e) {
-        // prevent default action and bubbling
-        e.preventDefault();
-        e.stopPropagation();
-        // set target to anchor's "href" attribute
-        var target = $(this).attr('href');
-        // scroll to each target
-        $(target).velocity('scroll', {
-            duration: 500,
-            easing: 'ease-in-out'
-        });
-    });
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
+$(document).ready(function(){
 	$('.animated').addClass('slideInDown');
 
 });
